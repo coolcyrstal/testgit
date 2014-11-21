e436adb4eb8e2d03e2e469ef935ef1b028197c2b
chayen <chayenjr.sk131@gmail.com>
2014-11-21 20:48:11 +0700

    add file

---
+++ gamelib.py
@@ -0,0 +1,32 @@
+import pygame
+from pygame.locals import *
+ 
+class SimpleGame(object):
+ 
+    def __init__(self, title, window_size=(640,480), fps=60):
+        self.title = title
+        self.window_size = window_size
+        self.fps = fps
+
+    def _game_init(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.surface = pygame.display.set_mode(self.window_size)
+        pygame.display.set_caption(self.title)
+        self.font = pygame.font.SysFont("monospace", 20)
+ 
+    def run(self):
+        self.init()
+        while True:
+            self.update()
+            self.render()
+            self.clock.tick(self.fps)
+ 
+    def init(self):
+        self._game_init()
+ 
+    def update(self):
+        pass
+ 
+    def render(self):
+        pass
---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+from gamelib import SimpleGame
+ 
+class SquashGame(SimpleGame):
+    pass
+ 
+def main():
+    game = SquashGame('SquashGame')
+    game.run()
+ 
+if __name__ == '__main__':
+    main()
+    ###
